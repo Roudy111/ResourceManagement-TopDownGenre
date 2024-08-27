c2a3f1042abe713d76523396aa4a57739cf69d10
Roudy111 <142318187+Roudy111@users.noreply.github.com>
2024-08-27 17:51:45 +0200

    Base Game
    It has all functions and base scripts for further developments

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public ColorPicker ColorPicker;
+
+    public void NewColorSelected(Color color)
+    {
+        // add code here to handle when a color is selected
+        MainManager.instance.TeamColor = color;
+    }
+
+
+    private void Start()
+    {
+        ColorPicker.Init();
+        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.instance.TeamColor);
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+
+        MainManager.instance.LoadColor();    
+        ColorPicker.SelectColor(MainManager.instance.TeamColor);
+
+    }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void exit()
+    {
+#if UNITY_EDITOR  
+
+    EditorApplication.ExitPlaymode();
+    MainManager.instance.SaveColor();
+
+#else
+
+    application.quit
+
+#endif
+
+    }
+}
